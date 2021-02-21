4e30021d5516dc6f9a0721a1b12022f83754d72d
xMalixoNeQ <79415120+xMalixoNeQ@users.noreply.github.com>
2021-02-21 18:08:05 +0100

    Add files via upload

---
+++ DiscordBot/main.rb
@@ -0,0 +1,11 @@
+require 'discordrb'
+config = File.foreach('config.txt').map { |line| line.split(' ').join(' ') }
+bottoken = config[0].to_s
+bot = Discordrb::Commands::ComandBot.new token: "#{bottoken}", client_id: "#{config[1].to_s}", prefix: "#{config[2].to_s}",
+
+bot.message(content: 'ping') do |event|
+   event.respont 'Pong!'
+end    
+/ Aby Startować bota wpisz ruby main.rb /
+at_exit ( bot.stop )
+bot.run
